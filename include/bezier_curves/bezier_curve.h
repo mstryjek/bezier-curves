85e4c084c2c79f1bf203e9be2d887f477bbeb788
mstryjek <mateusz@wtp3.org>
2022-07-19 20:24:34 +0200

    header: Add header skeletons

---
+++ include/bezier_curves/bezier_curve.h
@@ -0,0 +1,21 @@
+/**
+ * @file bezier_curve.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#pragma once
+
+
+namespace Bezier{
+	// Abstract
+	class Curve{
+
+	};
+}
---
+++ include/bezier_curves/factorials.h
@@ -0,0 +1,13 @@
+/**
+ * @file factorials.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#pragma once
+
